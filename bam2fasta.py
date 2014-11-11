e9b97a669f69b65adbff4d742e7c031bda654810
Marco Previtali <marco.previtali@disco.unimib.it>
2014-11-11 14:58:21 +0100

    Improve reads clustering in bam2fasta.py

--- bam2fasta.py
+++ bam2fasta.py
@@ -11,24 +11,33 @@ from Bio import SeqIO, Seq, SeqRecord
 def main(in_file, outdir):
     if not os.path.exists(outdir):
 	os.makedirs(outdir)
-    out_file = "{0}/{1}.fa".format(outdir, os.path.splitext(os.path.basename(in_file))[0])
-    with open(out_file, "w") as out_handle:
-        # Write records from the BAM file one at a time to the output file.
-        # Works lazily as BAM sequences are read so will handle large files.
-        SeqIO.write(bam_to_rec(in_file), out_handle, "fasta")
+    clusters=[]
+    current_cluster=[]
 
-def bam_to_rec(in_file):
-    """Generator to convert BAM files into Biopython SeqRecords.
-    """
-    bam_file = pysam.Samfile(in_file, "rb")
-    read_num=0
+    bam_file=pysam.Samfile(in_file, "rb")
     for read in bam_file:
-        seq = Seq.Seq(read.seq)
-        if read.is_reverse:
-            seq = seq.reverse_complement()
-        rec = SeqRecord.SeqRecord(seq, "/read_num={1} /gb={0} /clone_end=5'".format(read.qname, read_num), "", "")
-	read_num += 1
-        yield rec
+	if len(current_cluster)==0 or current_cluster[-1].pos + current_cluster[-1].qlen > read.pos - 1000:
+		current_cluster.append(read)
+	else:
+		clusters.append(current_cluster)
+		current_cluster=[]
+    if len(current_cluster)!=0:
+	clusters.append(current_cluster)
+
+    cnum=0
+    for cluster in clusters:
+	if len(cluster) > 50:
+		outfilename="{0}/{1}.cluster{2}.fa".format(outdir, os.path.splitext(os.path.basename(in_file))[0], cnum)
+		outfile=open(outfilename, 'w')
+		read_num=0
+		for read in cluster:
+			seq = Seq.Seq(read.seq)
+			if read.is_reverse:
+				seq=seq.reverse_complement()
+			rec=SeqRecord.SeqRecord(seq, "/read_num={0} /gb={0} /clone_end=5' /name={1}".format(read_num, read.qname), "", "")
+			SeqIO.write(rec, outfile, "fasta")
+			read_num+=1
+		cnum+=1
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
