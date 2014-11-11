cac04b3f5a8dedc24293b8120e8e929382a3f34c
Marco Previtali <marco.previtali@disco.unimib.it>
2014-11-11 16:17:18 +0100

    Add argparse interface to bam2fasta.py

--- bam2fasta.py
+++ bam2fasta.py
@@ -4,40 +4,70 @@
 
 import os
 import sys
-
+import argparse
 import pysam
+
 from Bio import SeqIO, Seq, SeqRecord
 
-def main(in_file, outdir):
-    if not os.path.exists(outdir):
-	os.makedirs(outdir)
-    clusters=[]
-    current_cluster=[]
+def main( ):
+    parser = argparse.ArgumentParser( prog = "bam2fasta.py",
+                                      description = "Convert and cluster reads from bam",
+                                      formatter_class = argparse.ArgumentDefaultsHelpFormatter )
+    parser.add_argument( '-b', '--bam-file',
+                         help = "BAM file",
+                         required = False, dest = 'in_file',
+                         default = "-")
+    parser.add_argument( '-o', '--out-dir',
+                         help = "Reads length",
+                         required = False, dest = 'outdir' )
+    parser.add_argument( '-c', '--cluster-min-size',
+                         help = "Minimum cluster size",
+                         required = False, dest = 'minsize', type = int,
+                         default = 50 )
+    parser.add_argument( '-m', '--max-linkage',
+                         help = "Max distance between two different reads in the same cluster",
+                         required = False, dest = 'linkage', type = int,
+                         default = 2000 )
 
-    bam_file=pysam.Samfile(in_file, "rb")
+    args = parser.parse_args()
+    outdir = args.outdir if args.outdir is not "" else "output"
+    in_file = args.in_file
+    linkage = args.linkage
+    cluster_min_size = args.minsize
+
+    if not os.path.exists( outdir ):
+	os.makedirs( outdir )
+    clusters = []
+    current_cluster = []
+
+    bam_file = pysam.Samfile( in_file, "rb" )
     for read in bam_file:
-	if len(current_cluster)==0 or current_cluster[-1].pos + current_cluster[-1].qlen > read.pos - 1000:
-		current_cluster.append(read)
+	if len( current_cluster ) == 0 or current_cluster[-1].pos + current_cluster[-1].qlen > read.pos - linkage:
+            current_cluster.append( read )
 	else:
-		clusters.append(current_cluster)
-		current_cluster=[]
-    if len(current_cluster)!=0:
-	clusters.append(current_cluster)
+            clusters.append( current_cluster )
+            current_cluster = []
+    if len( current_cluster ) != 0:
+	clusters.append( current_cluster )
 
-    cnum=0
+    cnum = 0
     for cluster in clusters:
-	if len(cluster) > 50:
-		outfilename="{0}/{1}.cluster{2}.fa".format(outdir, os.path.splitext(os.path.basename(in_file))[0], cnum)
-		outfile=open(outfilename, 'w')
-		read_num=0
-		for read in cluster:
-			seq = Seq.Seq(read.seq)
-			if read.is_reverse:
-				seq=seq.reverse_complement()
-			rec=SeqRecord.SeqRecord(seq, "/read_num={0} /gb={0} /clone_end=5' /name={1}".format(read_num, read.qname), "", "")
-			SeqIO.write(rec, outfile, "fasta")
-			read_num+=1
-		cnum+=1
+	if len(cluster) > cluster_min_size:
+            outfile = open( "{0}/{1}.cluster{2}.fa".format( outdir,
+                                                            os.path.splitext( os.path.basename( in_file ) )[0] if in_file != "-" else "output",
+                                                            cnum ),
+                            'w' )
+            read_num = 0
+            for read in cluster:
+                seq = Seq.Seq( read.seq )
+                if read.is_reverse:
+                    seq = seq.reverse_complement()
+                rec = SeqRecord.SeqRecord( seq,
+                                           "/read_num={0} /gb={0} /clone_end=5' /name={1}".format( read_num, read.qname ),
+                                           "", "" )
+                SeqIO.write( rec, outfile, "fasta" )
+                read_num += 1
+            cnum += 1
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    main( )
